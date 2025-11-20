dca340cafd1247399ad68d8f2a35ba88bcff6113
sorin2net <denisdumitriu95@gmail.com>
2025-11-20 23:27:48 +0200

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,5 +3,17 @@ import { useState } from "react"
 
 export const ThemeToggle = () => {
     const [isDarkMode,setIsDarkMode]= useState(false);
-    return <button>{isDarkMode ? <Sun />: <Moon />} </button>
+
+    const toggleTheme = () => {
+        if(isDarkMode){
+            setIsDarkMode(false);
+    }
+
+
+        else{
+            setIsDarkMode(true);
+        }
+
 }
+    return <button onClick={toggleTheme}>{isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" />: <Moon className="h-6 w-6 text-blue-900"/>} </button>
+};
