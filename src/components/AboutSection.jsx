4fdec88ead91a30177f4d687dc25875f694edbe7
sorin2net <denisdumitriu95@gmail.com>
2025-11-22 01:01:47 +0200

    about-section-done

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,3 +1,4 @@
+import { Briefcase, Code, User } from "lucide-react";
 
 
 export const AboutSection= () => {
@@ -27,11 +28,62 @@ export const AboutSection= () => {
 
                     <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                         <a href="#contact" className="cosmic-button">
-                            Contact {/*aici vreau un document cu cv-ul in src si sa il pot descarca prin buton*/}
+                            Contact 
                         </a> 
-
+                        <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10
+                         transition-colors duration-300 text-center"
+                         >
+                           Descarca CV {/*aici vreau un document cu cv-ul in src si sa il pot descarca prin buton, acolo la href pun link-ul catre cv sa il downloadez*/}
+                        </a> 
                     </div>
                 </div>
+
+                <div className="grid grid-cols-1 gap-6">
+
+                    <div className="gradient-border p-6 card-hover">
+
+                        <div className="flex items-start gap-4">
+                            <div className="p-3 rounded-full bg-primary/10">
+                                <Code className="h-6 w-6 text-primary"/>
+                            </div>
+                            <div className="text-left">
+                                <h4 className="font-semibold text-lg"> Aplicatii c++/java/python </h4>
+                                <p className="text-muted-foreground"> imi place sa codez foarte mult fac asta si in timpul liber</p>
+                            </div> {/*de modificat*/}
+                        </div>
+                        
+                    </div >
+
+                    <div className="gradient-border p-6 card-hover"> 
+                        
+                        <div className="flex items-start gap-4">
+                            <div className="p-3 rounded-full bg-primary/10">
+                                <User className="h-6 w-6 text-primary"/>
+                            </div>
+                            <div className="text-left">
+                                <h4 className="font-semibold text-lg"> Aplicatii c++/java/python </h4>
+                                <p className="text-muted-foreground"> imi place sa codez foarte mult fac asta si in timpul liber</p>
+                            </div> {/*de modificat*/}
+                        </div>
+                        
+                    </div>
+
+                    <div className="gradient-border p-6 card-hover">
+                        
+                        <div className="flex items-start gap-4">
+                            <div className="p-3 rounded-full bg-primary/10">
+                                <Briefcase className="h-6 w-6 text-primary"/>
+                            </div>
+                            <div className="text-left">
+                                <h4 className="font-semibold text-lg"> Aplicatii c++/java/python </h4>
+                                <p className="text-muted-foreground"> imi place sa codez foarte mult fac asta si in timpul liber</p>
+                            </div> {/*de modificat*/}
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
             </div>
         </div>
      </section>
