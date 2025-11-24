d7d88a38855c1b6ee4c275e91b4ff747fd78aa4a
sorin2net <denisdumitriu95@gmail.com>
2025-11-24 02:50:32 +0200

    ProjectSection+imagini de pus acolo

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,42 @@
+const projects=[
+    {
+        id: 1,
+        title: "Cinema Java",
+        description: "Copie cinema city facut in java cu interfata grafica Swing",
+        imageUrl: "/projects/CinemaJava.JPG",
+        tags: ["Java", "Swing", "OOP"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+    {
+        id: 2,
+        title: "Logical",
+        description: "Proiect in baza de date pentru un cinema care vinde bilete",
+        imageUrl: "/projects/Logical.png",
+        tags: ["SQL", "Database", "PL/SQL"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+    {
+        id: 3,
+        title: "RedHood",
+        description: "Joc 2D facut in Java folosind OOP si design patterns",
+        imageUrl: "/projects/redhood.png",
+        tags: ["Java", "OOP", "Design Patterns"],
+        demoUrl: "#",
+        githubUrl: "#",
+    }
+]
+{/*mai sus trebuie sa adaug poze la ce proiecte fac, de preferat multiplu de 3, o sa incerc sa fac 6 sa fie*/}
+{/* */}
+
+export const ProjectsSection= () => {
+    return (
+        <section>
+
+
+        </section>
+    )
+
+
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { ThemeToggle } from "@/components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { AboutSection } from "@/components/AboutSection";    
 import { SkillsSection } from "@/components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
 
 export const Home= () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
@@ -20,6 +21,7 @@ export const Home= () => {
             <HeroSection />
             <AboutSection />
             <SkillsSection />
+            <ProjectsSection />
         </main>
         {/* Footer */}
 
